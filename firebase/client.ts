af5d694ae003f2e36989443f12bae837ee30e3f2
Tiwari Dhwani Kanhaiyalal <dhwani.tiwari11@gmail.com>
2025-04-24 21:53:56 +0530

    changed client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,6 +6,17 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Debug logging
+console.log('Firebase Environment Variables:', {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ? 'Present' : 'Missing',
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ? 'Present' : 'Missing',
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ? 'Present' : 'Missing',
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ? 'Present' : 'Missing',
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ? 'Present' : 'Missing',
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ? 'Present' : 'Missing',
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ? 'Present' : 'Missing'
+});
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
